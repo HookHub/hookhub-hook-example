ef902c87e46366ba80c2b874e35c5602e2c54c8d
Ty Eggen <tycho.eggen@gmail.com>
2018-09-30 14:39:44 -0700

    Updated default handler and comments

--- index.js
+++ index.js
@@ -4,10 +4,32 @@ var debug = require('debug')('hookhub-hook-example');
 
 debug("Loading");
 
-/* GET home page. */
+// Get Payload
+router.use('/', function (req, res, next) {
+	if (req.method == 'GET') {
+		res.locals.payload = req.query;
+	} else if (req.method == 'POST') {
+		debug("Request Body:");
+		debug(req.body);
+		if (req.headers['content-type'] == 'application/json' && typeof req.body != 'object') {
+			debug("Converting to JSON");
+			res.locals.payload = JSON.parse(req.body);
+		} else {
+			res.locals.payload = req.body;
+		}
+	}
+	next();
+});
+
+/* Default handler. */
 router.use('/', function(req, res, next) {
   debug("Handling default request");
-  res.send({result:"OK",message:"It works!"});
+  var params = {result:"OK",message:"It works!"};
+
+  if(res.locals.payload.message !== undefined)
+    params.message = res.locals.payload.message;
+
+  res.send(params);
 });
 
 module.exports = router;
